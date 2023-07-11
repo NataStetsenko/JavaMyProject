15380425f02c2a76a845ea51aee0052756fb2926
Natali <1979stetsenko@gmail.com>
2023-07-11 22:27:12 +0300

    HWork1

---
+++ src/main/java/step/learn/controls/ControlDemo.java
@@ -0,0 +1,29 @@
+package step.learn.controls;
+
+public class ControlDemo {
+    public void run(){
+        System.out.println("ControlDemo run");
+        new HomeWork1().print();
+
+
+
+        byte xb;
+        short xs;
+        int xi;
+        long xl;
+        float xf;
+        double xd;
+        boolean xbo;
+        char cx;
+        Byte b;
+        Short s;
+        Integer i;
+        Long l;
+        Float f;
+        Double d;
+        Boolean bo;
+        Character x;
+
+    }
+
+}
